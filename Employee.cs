93b2926cb1e3971de1f544d96bf524a183af9006
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:47:14 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo2
+{
+    public class Employee : IPrintable, IDisplay
+    {
+        void IPrintable.Print()
+        {
+            Console.WriteLine("Employee Class implemented Print Method of IPrintable");
+        }
+
+        void IPrintable.Show()
+        {
+            Console.WriteLine("Employee Class implemented Show Method of IPrintable");
+        }
+
+        void IDisplay.Display()
+        {
+            Console.WriteLine("Employee Class implemented Display Method of IDisplay");
+        }
+
+        void IDisplay.Show()
+        {
+            Console.WriteLine("Employee Class implemented Show Method of IDisplay");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee emp = new Employee();
+
+            //emp.
+
+            IPrintable empPrint = new Employee();
+            empPrint.Print();
+            empPrint.Show();
+
+            IDisplay empDisp = new Employee();
+            empDisp.Display();
+            empDisp.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
